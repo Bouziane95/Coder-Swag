718ef00011b1fd08765096c291206a2ec6070195
Bouziane <bey.bouziane@live.fr>
2019-07-02 17:55:18 +0200

    Shop by category done

---
+++ Coder-Swag/Coder-Swag/Services/DataServices.swift
@@ -0,0 +1,19 @@
+//
+//  DataServices.swift
+//  Coder-Swag
+//
+//  Created by Bouziane Bey on 02/07/2019.
+//  Copyright © 2019 Bouziane Bey. All rights reserved.
+//
+
+import Foundation
+
+class DataService{
+    static let instance = DataService()
+    
+    private let categories = [Category(title: "SHIRTS", imageName: "shirts.png"), Category(title: "HOODIES", imageName: "hoodies.png"), Category(title: "HATS", imageName: "hats.png"), Category(title: "DIGITAL", imageName: "digital.png")]
+    
+    func getCategories() -> [Category]{
+        return categories
+    }
+}
