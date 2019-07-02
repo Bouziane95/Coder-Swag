7d811a1e3f3d3ad0c3d3b5f330f04b736a7ba0fd
Bouziane <bey.bouziane@live.fr>
2019-07-02 15:27:44 +0200

    TableView Done

---
+++ Coder-Swag/Coder-Swag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  Coder-Swag
+//
+//  Created by Bouziane Bey on 02/07/2019.
+//  Copyright © 2019 Bouziane Bey. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
